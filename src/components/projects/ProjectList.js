9c870ff63acf4a28087bebbc1fab93ae89853026
mbwakeley <44876447+mbwakeley@users.noreply.github.com>
2020-03-30 20:47:59 -0700

    dashboard completed

---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+export default function ProjectList() {
+  return (
+    <div className="project-list selction">
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by Mark W</p>
+          <p className="grey-text">March 30th 8pm</p>
+        </div>
+      </div>
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by Mark W</p>
+          <p className="grey-text">March 30th 8pm</p>
+        </div>
+      </div>
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by Mark W</p>
+          <p className="grey-text">March 30th 8pm</p>
+        </div>
+      </div>
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by Mark W</p>
+          <p className="grey-text">March 30th 8pm</p>
+        </div>
+      </div>
+    </div>
+  );
+}
