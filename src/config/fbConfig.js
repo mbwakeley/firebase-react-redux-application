39c4ecc9c20e107cfd91d49394ec13bff92800e8
mbwakeley <44876447+mbwakeley@users.noreply.github.com>
2020-04-05 11:02:09 -0700

    adding data to firestore

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -4,14 +4,14 @@ import "firebase/auth";
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyCtdhncutoFq5g9kJ-KCP_ILMrAhADZm9E",
-  authDomain: "react-redux-firebase-app-6e2cb.firebaseapp.com",
-  databaseURL: "https://react-redux-firebase-app-6e2cb.firebaseio.com",
-  projectId: "react-redux-firebase-app-6e2cb",
-  storageBucket: "react-redux-firebase-app-6e2cb.appspot.com",
-  messagingSenderId: "288809015883",
-  appId: "1:288809015883:web:37dc32c588478c3cae4bce",
-  measurementId: "G-RT9ZQY6GZW"
+  apiKey: "AIzaSyBEkEwXLeZUn5N3Trp3R-v9oXEOm1S3ils",
+  authDomain: "react-redux-firebase-app-30fd9.firebaseapp.com",
+  databaseURL: "https://react-redux-firebase-app-30fd9.firebaseio.com",
+  projectId: "react-redux-firebase-app-30fd9",
+  storageBucket: "react-redux-firebase-app-30fd9.appspot.com",
+  messagingSenderId: "112756699767",
+  appId: "1:112756699767:web:aa17869f529b1b868bcc72",
+  measurementId: "G-V0PW061YWR"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
